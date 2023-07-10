aa09a77f72de89b6602fdcab768e37651891da0c
Ralph Yozzo <ralph@brooklynmarathon.com>
2023-07-10 17:25:40 -0400

    dell

--- runall_sync.sh
+++ runall_sync.sh
@@ -1,6 +1,12 @@
 #!
 
 # gsutil rsync gs://citytri-marketing.appspot.com/brooklyn-beach-half-marathon-10k-5k-2023-june-04/abigail-mccleary-2023-06-04-WINDOW--1-1.jpg abigail-mccleary/
-mkdir -p names/abigail-mccleary
-gsutil cp gs://citytri-marketing.appspot.com/brooklyn-beach-half-marathon-10k-5k-2023-june-04/abigail-mccleary-2023-06-04-WINDOW--1-1.jpg names/abigail-mccleary
-python -m   smugcli.smugcli sync  names/abigail-mccleary /names
+
+# mkdir -p names/abigail-mccleary
+# gsutil cp gs://citytri-marketing.appspot.com/brooklyn-beach-half-marathon-10k-5k-2023-june-04/abigail-mccleary-2023-06-04-WINDOW--1-1.jpg names/abigail-mccleary
+
+
+
+gsutil ls gs://citytri-marketing.appspot.com/brooklyn-beach-half-marathon-10k-5k-2023-june-04/** |grep WINDOW |awk -F/ '{dest=$NF; gsub("-2023.*","",dest); printf "mkdir -p names/%s\ngsutil cp gs://citytri-marketing.appspot.com/brooklyn-beach-half-marathon-10k-5k-2023-june-04/%s names/%s\n",dest,$NF,dest}'  |bash
+
+# python -m   smugcli.smugcli sync  names/abigail-mccleary /names
